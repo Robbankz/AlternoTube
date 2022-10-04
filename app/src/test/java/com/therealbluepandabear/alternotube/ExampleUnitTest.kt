bf3bc0ef37aa7404383ad8f42dce12578b1aec94
therealbluepandabear <justaguyoney@gmail.com>
2022-10-05 11:44:32 +1300

    Added local unit tests that test against whether or not the video source (mp4) is correct for the URL.

--- app/src/test/java/com/therealbluepandabear/alternotube/ExampleUnitTest.kt
+++ app/src/test/java/com/therealbluepandabear/alternotube/ExampleUnitTest.kt
@@ -1,17 +1,28 @@
 package com.therealbluepandabear.alternotube
 
+import com.therealbluepandabear.alternotube.models.RumbleScraper
 import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+    private val rumbleScraper = RumbleScraper.create()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun videoSrcIsCorrectForUrl_1() {
+        val videoSrc = rumbleScraper.scrapeVideoSrcFromUrl("https://rumble.com/v1ieskt-modded-minecraft-live-stream-ep48-space-training-modpack-lets-play-rumble-e.html")
+        assertEquals("https://sp.rmbl.ws/s8/2/T/4/N/C/T4NCf.caa.rec.mp4", videoSrc)
+    }
+
+    @Test
+    fun videoSrcIsCorrectForUrl_2() {
+        val videoSrc = rumbleScraper.scrapeVideoSrcFromUrl("https://rumble.com/v1mh9nz-how-many-takes-jeremy-lynch-football-bloopers-.html")
+        assertEquals("https://sp.rmbl.ws/s8/2/V/u/S/2/VuS2f.caa.mp4", videoSrc)
+    }
+
+    @Test
+    fun videoSrcIsCorrectForUrl_3() {
+        val videoSrc = rumbleScraper.scrapeVideoSrcFromUrl("https://rumble.com/v1mll2a-stay-free-with-russell-brand-005-what-is-a-fascist-and-who-decides.html")
+        assertEquals("https://sp.rmbl.ws/s8/2/I/F/D/3/IFD3f.caa.rec.mp4", videoSrc)
     }
 }
