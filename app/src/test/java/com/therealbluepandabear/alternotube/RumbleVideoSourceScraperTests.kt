52d9a3c6149549ab70aad51e4f05985ef7ab28c6
therealbluepandabear <justaguyoney@gmail.com>
2022-10-06 18:04:55 +1300

    Clean up `RumbleVideoSourceScraperTests`.

--- app/src/test/java/com/therealbluepandabear/alternotube/RumbleVideoSourceScraperTests.kt
+++ app/src/test/java/com/therealbluepandabear/alternotube/RumbleVideoSourceScraperTests.kt
@@ -10,19 +10,22 @@ class RumbleVideoSourceScraperTests {
 
     @Test
     fun videoSrcIsCorrectForUrl_1() {
-        val videoSrc = rumbleScraper.scrapeVideoSource("https://rumble.com/v1ieskt-modded-minecraft-live-stream-ep48-space-training-modpack-lets-play-rumble-e.html").data
-        assertEquals("https://sp.rmbl.ws/s8/2/T/4/N/C/T4NCf.caa.rec.mp4", videoSrc)
+        val jsoupResponse = rumbleScraper.scrapeVideoSource("https://rumble.com/v1ieskt-modded-minecraft-live-stream-ep48-space-training-modpack-lets-play-rumble-e.html")
+        val videoSource = jsoupResponse.data
+        assertEquals("https://sp.rmbl.ws/s8/2/T/4/N/C/T4NCf.caa.rec.mp4", videoSource)
     }
 
     @Test
     fun videoSrcIsCorrectForUrl_2() {
-        val videoSrc = rumbleScraper.scrapeVideoSource("https://rumble.com/v1mh9nz-how-many-takes-jeremy-lynch-football-bloopers-.html").data
-        assertEquals("https://sp.rmbl.ws/s8/2/V/u/S/2/VuS2f.caa.mp4", videoSrc)
+        val jsoupResponse = rumbleScraper.scrapeVideoSource("https://rumble.com/v1mh9nz-how-many-takes-jeremy-lynch-football-bloopers-.html")
+        val videoSource = jsoupResponse.data
+        assertEquals("https://sp.rmbl.ws/s8/2/V/u/S/2/VuS2f.caa.mp4", videoSource)
     }
 
     @Test
     fun videoSrcIsCorrectForUrl_3() {
-        val videoSrc = rumbleScraper.scrapeVideoSource("https://rumble.com/v1mll2a-stay-free-with-russell-brand-005-what-is-a-fascist-and-who-decides.html").data
-        assertEquals("https://sp.rmbl.ws/s8/2/I/F/D/3/IFD3f.caa.rec.mp4", videoSrc)
+        val jsoupResponse = rumbleScraper.scrapeVideoSource("https://rumble.com/v1mll2a-stay-free-with-russell-brand-005-what-is-a-fascist-and-who-decides.html")
+        val videoSource = jsoupResponse.data
+        assertEquals("https://sp.rmbl.ws/s8/2/I/F/D/3/IFD3f.caa.rec.mp4", videoSource)
     }
 }
