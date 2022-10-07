798c294dcb85879edac6a30353060ef635f1fd90
therealbluepandabear <justaguyoney@gmail.com>
2022-10-08 11:15:21 +1300

    Fix #2.

--- app/src/main/java/com/therealbluepandabear/alternotube/models/RumbleScraper.kt
+++ app/src/main/java/com/therealbluepandabear/alternotube/models/RumbleScraper.kt
@@ -1,6 +1,5 @@
 package com.therealbluepandabear.alternotube.models
 
-import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.JsonParser
 import org.jsoup.Jsoup
@@ -25,39 +24,39 @@ class RumbleScraper private constructor() {
             val url = if (page <= 1) "${RUMBLE_URL}search/video?q=$query" else "${RUMBLE_URL}search/video?q=$query&page=$page"
             val document = Jsoup.connect(url).get()
 
-            if (document.getElementsByClass("video-listing-entry").size == 0) {
+            if (document.select("li.video-listing-entry").size == 0) {
                 return JsoupResponse(null, emptyList())
             } else {
-                for (element in document.getElementsByClass("video-listing-entry")) {
+                for (element in document.select("li.video-listing-entry")) {
                     val searchResult = RumbleSearchResult("", RumbleChannel("", 0, false), 0, "", "")
 
-                    for (element2 in element.getElementsByClass("video-item--title")) {
+                    for (element2 in element.select("h3.video-item--title")) {
                         searchResult.title = element2.text()
                     }
 
-                    for (element2 in element.getElementsByClass("video-item--by")) {
-                        for (element3 in element2.getElementsByClass("ellipsis-1")) {
+                    for (element2 in element.select("address.video-item--by")) {
+                        for (element3 in element2.select("div.ellipsis-1")) {
                             searchResult.channel.name = element3.text()
 
-                            if (element3.getElementsByClass("video-item--by-verified verification-badge-icon").isNotEmpty()) {
+                            if (element3.select("svg.video-item--by-verified verification-badge-icon").isNotEmpty()) {
                                 searchResult.channel.isVerified = true
                             }
                         }
                     }
 
-                    for (element2 in element.getElementsByClass("video-item--meta video-item--views")) {
+                    for (element2 in element.select("span.video-item--meta video-item--views")) {
                         if (element2.attr("data-value") != "") {
                             searchResult.views = element2.attr("data-value").toString().replace(",", "").toInt()
                         }
                     }
 
-                    for (element2 in element.getElementsByClass("video-item--img")) {
+                    for (element2 in element.select("img.video-item--img")) {
                         if (element2.attr("src") != "") {
                             searchResult.thumbnailSrc = element2.attr("src").toString()
                         }
                     }
 
-                    for (element2 in element.getElementsByClass("video-item--a")) {
+                    for (element2 in element.select("a.video-item--a")) {
                         if (element2.attr("href") != "") {
                             searchResult.videoUrl = "${RUMBLE_URL}${element2.attr("href")}"
                         }
@@ -82,7 +81,7 @@ class RumbleScraper private constructor() {
         try {
             val document = Jsoup.connect("${RUMBLE_URL}$id").get()
 
-            for (element in document.getElementsByTag("script")) {
+            for (element in document.select("script")) {
                 if (element.attr("type") == "application/ld+json") {
                     val content = element.data()
                     val array = JsonParser.parseString(content).asJsonArray
@@ -99,7 +98,7 @@ class RumbleScraper private constructor() {
                     }
 
                     val doc = Jsoup.connect("$RUMBLE_API_URL${embedId.reversed()}").ignoreContentType(true).get()
-                    val jsonData = doc.getElementsByTag("body").first()?.text()
+                    val jsonData = doc.select("body").first()?.text()
 
                     val mp4 = JsonParser.parseString(jsonData).asJsonObject.get("u").asJsonObject.get("mp4").asJsonObject.get("url").toString()
 
@@ -123,15 +122,14 @@ class RumbleScraper private constructor() {
             val channel = RumbleChannel(null, 0, false)
             val video = RumbleVideo("", channel, 0, 0, "")
 
-            channel.name = document.getElementsByClass("media-heading-name").first()?.text()
-            channel.isVerified = document.getElementsByClass("verification-badge-icon media-heading-verified").isNotEmpty()
+            channel.name = document.select("span.media-heading-name").first()?.text()
+            channel.isVerified = document.select("svg.verification-badge-icon media-heading-verified").isNotEmpty()
 
             video.title = document.title()
-            video.rumbles = RumbleScraperUtils.convertShorthandNumberToInt(document.getElementsByClass("rumbles-vote").first()?.getElementsByClass("rumbles-count")?.first()?.text().toString())
+            video.rumbles = RumbleScraperUtils.convertShorthandNumberToInt(document.select("div.rumbles-vote").first()?.select("span.rumbles-count")?.first()?.text().toString())
 
-            Log.d("BEPPER", document.getElementsByClass("rumbles-vote").first()?.getElementsByClass("rumbles-count")?.first()?.text().toString())
-            if (document.getElementsByClass("media-description").isNotEmpty()) {
-                for (element in document.getElementsByClass("media-description")) {
+            if (document.select("p.media-description").isNotEmpty()) {
+                for (element in document.select("p.media-description")) {
                     video.description += "${element.text()}\n"
                 }
             }
