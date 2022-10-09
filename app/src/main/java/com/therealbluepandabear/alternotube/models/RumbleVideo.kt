927981cdb3213a9d3027c2ef11cd22abd9f987d6
therealbluepandabear <justaguyoney@gmail.com>
2022-10-09 13:28:12 +1300

    Code adjustment.

--- app/src/main/java/com/therealbluepandabear/alternotube/models/RumbleVideo.kt
+++ app/src/main/java/com/therealbluepandabear/alternotube/models/RumbleVideo.kt
@@ -2,8 +2,8 @@ package com.therealbluepandabear.alternotube.models
 
 data class RumbleVideo(
     var title: String? = null,
-    var channel: RumbleChannel? = null,
     var thumbnailSrc: String? = null,
+    var channel: RumbleChannel? = null,
     var views: Int? = null,
     var rumbles: Int? = null,
     var uploadDate: String? = null,
